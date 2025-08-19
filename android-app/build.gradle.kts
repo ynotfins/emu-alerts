8b823d84fde2862d1d22c8427c8e410467795bd5
ynotfins <48276983+ynotfins@users.noreply.github.com>
2025-08-19 00:48:16 -0400

    fix(gradle): add pluginManagement repos and rename project to EMU Alerts

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 tasks.register("clean", Delete::class) {
